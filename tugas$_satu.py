6e6304db605b6072dc0ee2715238422b15b8a720
riskirizaldi <107458758+riskirizaldi@users.noreply.github.com>
2022-06-14 13:24:16 +0700

    Add files via upload

---
+++ tugas$_satu.py
@@ -0,0 +1,14 @@
+
+
+
+habits = []
+
+bangun = input('jam berapa bangun :')
+mandi = input('ke kamar mandi :')
+berpakaian = input('pakaian apa  :')
+makan = input('makan apa : ')
+
+
+keseharian = [bangun,mandi,berpakaian,makan]
+
+print(keseharian)
